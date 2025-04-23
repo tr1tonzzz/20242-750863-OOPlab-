0f24e4f1a9b392ee73211cccd2b89df66b14542d
tr1tonzzz <nampk20@gmail.com>
2025-04-23 20:40:06 +0700

    Add files via upload

---
+++ Lab03/Media.java
@@ -0,0 +1,51 @@
+package hust.soict.hedspi.aims.media;
+public abstract class Media {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    public Media(int id, String title, String category, float cost) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+    public boolean equals(Object o) {
+        if (this == o) return true; 
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Media media = (Media) o;
+        return this.title != null && this.title.equals(media.getTitle());
+    }
+}
